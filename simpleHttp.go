2ae3ab505e472787b827099043521c486af4ae7f
Fabian Brash <fabianbrash@gmail.com>
2020-08-21 12:13:29 -0400

    initial commit

---
+++ simpleHttp.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Println("Listening on port 0.0.0.0:8080...")
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
